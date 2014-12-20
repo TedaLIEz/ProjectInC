5449c61d0ee1345331961a96d3ae35aea81f6351
Jian Guo <guojian0102@hotmail.com>
2014-12-20 20:09:51 +0800

    Snake v2.5b

--- Snake.c
+++ Snake.c
@@ -1,15 +1,19 @@
 /**	This a Program to make a classic snake game
-*	@SNAKE define the snake's location
+*	2014.12.20 @author aLIEzTED
+*   @SNAKE define the snake's location
+*   @USER define the user's name AND score
+*   Have Fun!
 */
 
 
 #include<stdio.h>
+#include <time.h>
 #include<stdlib.h>
 #include<string.h> 
 #include <windows.h>
 #include <dos.h>
 #define ENTER 13
-#define ESC   0x011
+#define ESC   27
 #define RIGHT 77
 #define LEFT 75
 #define UP 72
@@ -19,17 +23,19 @@ typedef struct SNAKE{
 	int x;
 	int y;
 }Snake;
-Snake *head;
 typedef struct USER{
 	char username[20];
-	int score;
+	int userscore;
 }User;
+
+Snake *head;
 User *user;
 int pch,cch;
 int foodx[2];
 int poisonx[2];
 int minex[2];
-int score=0; 
+int score; 
+int userCount=0;
 int speed;
 int sLv;
 char pause;
@@ -42,7 +48,7 @@ void mine(int minex[2]);
 void displaymine(int foodx[2]);
 void food(int foodx[2]);
 void displayfood(int foodx[2]);
-void poison(int poisonx[2]);
+void poison(int poisonx[2],int score);
 void displaypoison(int poisonx[2]);
 void insertnew(Snake *head,int x,int y);
 void Map();
@@ -50,7 +56,20 @@ void Over();
 void record();
 void setlog();
 void restart();
+void sort();
+void game();
+char* getTime();
 
+/*
+*getCurrentTime
+*/
+char* getTime(){
+	time_t t;
+	t=time(NULL);
+	char *time;
+	time=ctime(&t);
+	return time;
+}
 void gotoxy(int x,int y)  
 {  
     COORD P;  
@@ -63,20 +82,46 @@ void HideCursor()
  CONSOLE_CURSOR_INFO cursor_info = {1, 0}; 
  SetConsoleCursorInfo(GetStdHandle(STD_OUTPUT_HANDLE), &cursor_info);
 }
-
+/*
+*   Print the Map
+*/
+void Map()				
+{
+	unsigned i,j;
+	gotoxy(10,0);
+	for(j=0;j<50;j++)
+		printf("*");
+	for(i=1;i<25;i++)
+	{
+		gotoxy(10,i);
+		for(j=0;j<50;j++)
+		{
+			if(j==0||j==49)
+				printf("*");
+			else
+				printf(" ");
+		}
+	}
+	gotoxy(10,i);
+	for(j=0;j<50;j++)
+		printf("*");
+}
 
 void init()
 {
+	score=0;
 	HideCursor();
-	printf("选择难度:\n1代表简单\n2代表中等\n3代表难");
+	printf("选择难度:\n1代表简单\n2代表中等\n3代表难\n其他键退出");
 	int lv; 
 	scanf("%d",&lv);
 	if(lv==1){
-		speed=500;
-	}else if(lv==2){
 		speed=400;
-	}else if(lv==3){
+	}else if(lv==2){
 		speed=300;
+	}else if(lv==3){
+		speed=250;
+	}else{
+		exit(0);
 	}
 	system("cls"); 
 	displayMessage();
@@ -90,16 +135,14 @@ void init()
 	insertnew(head,41,10);
 	insertnew(head,42,10);
 	displaySnake(head);
-	
-	poison(poisonx);
-	displaypoison(poisonx);
+	poison(poisonx,score);
 	food(foodx);
 	displayfood(foodx);
 	mine(minex);
 	displaymine(minex);
 	displayscore();	    
     gotoxy(65,13);  
-	pch=cch=UP; 
+	pch=cch=LEFT; 
 }                          
 void displayMessage()
 {
@@ -130,25 +173,28 @@ void displayscore()
 	printf("总得分为%d",score);
 	gotoxy(65,14);
 	if(score==1){
-		printf("FIRST BLOOD");
+		printf("FIRST BLOOD   ");
 	}else if(score==3){
-		printf("KILLING SPREE");
+		printf("KILLING SPREE ");
 	}else if(score==4){
-		printf("DOMINATING");
+		printf("DOMINATING    ");
 	}else if(score==5){
-		printf("MEGA-KILL");
+		printf("MEGA-KILL     ");
 	}else if(score==6){
-		printf("UNSTOPPABLE");
+		printf("UNSTOPPABLE   ");
 	}else if(score==7){
-		printf("WICKED SICK");
+		printf("WICKED SICK   ");
 	}else if(score==8){
-		printf("MONSTER KILL");
+		printf("MONSTER KILL  ");
 	}else if(score==9){
-		printf("GODLIKE");
+		printf("GODLIKE       ");
 	}else if(score>=10){
 		printf("BEYOND GODLIKE");
 	}
-}                    //Score List
+}                   
+/*
+*Print the Snake
+*/
 void displaySnake(Snake *head)
 {
 	Snake* first=head;
@@ -161,7 +207,10 @@ void displaySnake(Snake *head)
 			printf("*");
 		first=first->next;	
 	}
-}                    //Print Snake
+}                   
+/*
+*Make the Mine,Food,poison randomly AND display them.
+*/
 void mine(int minex[2])  
 {  
     srand((unsigned)time(NULL));  
@@ -193,25 +242,38 @@ void displayfood(int foodx[2])
     gotoxy(foodx[0],foodx[1]);  
     printf("&");  
 }      //Print Food
-void poison(int poisonx[2])
+void poison(int poisonx[2],int score)
 {
-	int i=0;
-	srand((unsigned)time(NULL));
-	poisonx[0]=rand()%24+11;
-	poisonx[1]=rand()%12+1;
-	while(!((poisonx[0]>10)&&(poisonx[0]<60)&&(poisonx[1]>0)&&(poisonx[1]<25))){
-			displaypoison(poisonx);
+	if(score>5&&score<10){
+		int i=0;
+		srand((unsigned)time(NULL));
+		poisonx[0]=rand()%24+11;
+		poisonx[1]=rand()%12+1;
+		while(!((poisonx[0]>10)&&(poisonx[0]<60)&&(poisonx[1]>0)&&(poisonx[1]<25))){
 			poisonx[0]=rand()%24+11;
 			poisonx[1]=rand()%12+1;
-			displaypoison(poisonx);
-		
+		}
+		displaypoison(poisonx);
 	}
+	
 }                      //Make Poison
 void displaypoison(int poisonx[2])
 {
 	gotoxy(poisonx[0],poisonx[1]);
 	printf("!");
 }               //Print Poison
+
+
+void myclear(Snake *head)  
+{  
+    while(head!=NULL)  
+    {  
+        gotoxy(head->x,head->y);  
+        printf(" ");  
+        head=head->next;  
+        
+    }  
+}        
 void insertnew(Snake *head,int x,int y)
 {
 	Snake *newlnk;
@@ -226,6 +288,9 @@ void insertnew(Snake *head,int x,int y)
 	first->next=newlnk;
 	newlnk->next=NULL;
 }     //New Snake
+/*
+*	EventListener
+*/
 void eatfood(Snake *head)
 {
 	Snake *newlink,*current;
@@ -241,24 +306,20 @@ void eatfood(Snake *head)
 		newlink->x=current->x;
 		newlink->y=current->y;
 		newlink->next=NULL;
-		
 		score++;
 		if(score>5){
 			sLv++;
-			speed-=100;
+			speed-=25;
 		}else if(score>10){
 			sLv++;
-			speed-=100;
-		}else if(score>15){
-			sLv++;
-			speed-=100;
+			speed-=50;
 		}
         food(foodx);
 		displayfood(foodx);
-		
+		poison(poisonx,score);
 	}
 	
-}                            //isEatFood 
+}                             
 void eatpoison(Snake *head)
 {
 	Snake *current=head;
@@ -273,12 +334,11 @@ void eatpoison(Snake *head)
 		}
 	
 		current->next=NULL;
-		poison(poisonx);
-		displaypoison(poisonx);
+		poison(poisonx,score);
 		score--;
 	}
 	
-}                        //isEatPoison
+}                        
 void eatMine(Snake *head){
 	Snake *current;
 	int count=0; 
@@ -306,28 +366,10 @@ void eatMine(Snake *head){
 		displaymine(minex);
 		
 	}
-}                       //isEatMine
-void Map()				//Print Map
-{
-	unsigned i,j;
-	gotoxy(10,0);
-	for(j=0;j<50;j++)
-		printf("*");
-	for(i=1;i<25;i++)
-	{
-		gotoxy(10,i);
-		for(j=0;j<50;j++)
-		{
-			if(j==0||j==49)
-				printf("*");
-			else
-				printf(" ");
-		}
-	}
-	gotoxy(10,i);
-	for(j=0;j<50;j++)
-		printf("*");
-}
+}                       
+/*
+* Check whether head meets tail
+*/
 int test(Snake *head)
 {
 	Snake *first=head->next;
@@ -340,7 +382,10 @@ int test(Snake *head)
 		first=first->next;
 	}
 	return 1;
-}        //Test 
+}        
+/*
+*  KeyBoardListener
+*/
 int button()  
 {  
     int ch;  
@@ -352,17 +397,9 @@ int button()
     	}
     }  
     return ch; 
-}  										//cin 
-void myclear(Snake *head)  
-{  
-    while(head!=NULL)  
-    {  
-        gotoxy(head->x,head->y);  
-        printf(" ");  
-        head=head->next;  
-        
-    }  
-}        //del
+}
+  									
+
 void move(Snake*head)
 {
 	int x1,x2,y1,y2,flag=0;
@@ -432,10 +469,26 @@ void move(Snake*head)
 		head=head->next;
 	}					//KeepMoving 
 }
+
+void restart(){
+	gotoxy(22,12);
+	printf("Do You Want to Play Again?  Y/N");
+	char c;
+	c=getch();
+	if(c=='y'){
+		gotoxy(0,0);
+		system("cls");
+		init();
+		game();
+	}else if(c=='n'){
+		exit(0);
+	}
+}		
+
 void game()
 {
 	int flag=0;
-		while((head->x>10)&&(head->x<60)&&(head->y>0)&&(head->y<25))  
+	while((head->x>10)&&(head->x<59)&&(head->y>0)&&(head->y<25))  
     {  
       	if(score<0)
 			break;
@@ -478,45 +531,79 @@ void game()
     	}
     	Over();
 		record();
-		restart(); 
+		
 		if(cch==ESC){
 			exit(0);
 		} 
 }				
-void restart(){
-	system("cls");
-	gotoxy(25,12);
-	printf("Do You Want to Play Again?  Y/N");
-	if(getch()=='y'||'Y'){
-		gotoxy(0,0);
-		system("cls");
-		init();
-		game();
-	}else if(getch()=='n'||'N'){
-		exit(0);
-	} 
-}		
+
 void Over(){
 	gotoxy(30,12); 
 	printf("GAME OVER!!!");  
 }
 void record(){
+	static int flag=0; 
+	char cScore[20];
+	itoa(score,cScore,10);
+	FILE *fp;
+	
+	if(!flag){
+		user=(User*)malloc(sizeof(User));	
+		flag=1;
+	}
+	if(flag==1){
+		user=(User*)realloc(user,sizeof(User));
+	}
+	
 	if(score<0){
 		score=0;
 	}
-	char cScore[20];
-	int i;
-	itoa(score,cScore,10);
+	
 	gotoxy(30,13);
 	printf("输入姓名");
-	FILE *fp;
 	scanf("%s",usrname);
 	fp=fopen("record.txt","a");
 	fputs(usrname,fp);
 	fputs(" Score",fp);
 	fputs(cScore,fp);
+	fprintf(fp,"\n",fp);
 	fclose(fp);
-	
+	system("cls");
+	strcpy(user[userCount].username,usrname);
+	user[userCount].userscore=score;
+	userCount++;
+	sort(user,userCount);
+	restart();
+}
+void sort(User *user,int userCount){
+	gotoxy(25,13);
+	printf("----------英雄榜----------");
+	gotoxy(25,14);
+	printf("Player");
+	gotoxy(46,14);
+	printf("Score");
+	int i,j,k,t;
+	char str[30];
+	for(i=0;i<userCount+1;i++)
+	{
+		k=i;
+		for(j=i+1;j<userCount;j++)
+			if(user[j].userscore<user[k].userscore)
+				k=j;
+				t=user[k].userscore;
+				strcpy(str,user[k].username);
+				user[k].userscore=user[i].userscore;
+				strcpy(user[k].username,user[i].username);
+				user[i].userscore=t;
+				strcpy(user[i].username,str);
+				
+	}
+	for(i=0;i<userCount;i++){
+		gotoxy(25,15+i);
+		printf("%s",user[i].username);
+		gotoxy(46,15+i);
+		printf("%d",user[i].userscore);	
+	}
 }
 void setlog(){
 	char cScore[20];
@@ -525,6 +612,7 @@ void setlog(){
 	itoa(score,cScore,10);
 	FILE *log;
 	log=fopen("log.txt","a");
+	fprintf(log,"%s\n",getTime());
 	fputs("Current Score",log);
 	fputs(cScore,log);
 	fputs("Current Level",log);
@@ -535,7 +623,6 @@ int main()
 {
 		init();
 		game();
-		//record();
 		return 0;
 }
 
